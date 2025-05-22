0acdd4aa08f2fe833b184194f998a2bec41df312
plusk0 <pluskovich@outlook.com>
2025-05-22 14:34:43 +0200

    Updated main.py to accept a path variable. Added Exceptions

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from stats import output
+import sys
 import time
 
-def main():
-    start = time.time()
-    path = "books/frankenstein.txt"
-    print(output(path))
-    end = time.time()
-    print(end - start)
-    return 0
-main()
+def main(path):
+    try:
+        filepath = path[1]
+        start = time.time()
+        #path = "books/frankenstein.txt"
+        print(output(filepath))
+        end = time.time()
+        print(end - start)
+        return 0
+    except Exception as e:
+        print("Usage: python3 main.py <path_to_book>")
+    exit(1)
+main(sys.argv)
--- stats.py
+++ stats.py
@@ -1,49 +1,49 @@
 import time
 
-def get_num_words(filepath):
+def get_num_words(contents):
 
-    with open(filepath) as f:
-        contents = f.read()
         word_list = contents.split()
         return len(word_list)
     
-def get_sorted_list(filepath):
+def get_sorted_list(contents):
+
     letter_table = {}
-    with open(filepath) as f:
-        word_list = f.read()
-    for char in word_list:
+    for char in contents:
         if char.isalpha():
             char = char.lower()
             if char in letter_table:
                 letter_table[char] += 1
             else:
                 letter_table[char] = 1
-    sorted_list = []
 
+    sorted_list = []
     for entry in letter_table:
         sorted_list.append({"char": entry, "number":letter_table[entry]})
     sorted_list.sort(reverse=True, key=lambda a: a["number"])
-    print(sorted_list)
+    #print(sorted_list)
     return sorted_list
 
 
 def format(sorted_list):
     string = ""
     for entry in sorted_list:
-        
-        string = str(string + str(entry["char"])+ ": " + str(entry["number"]) + "\n")
+        string = string + str(entry["char"]) + ": " + str(entry["number"]) + "\n"
     return string
 
 
 
 def output(filepath):
-    num_words = get_num_words(filepath)
-    output_list = (format(get_sorted_list(filepath)))
+    print(filepath)
+    with open(filepath) as f:           # with statement for file opening helps with correctly closing the file -> resource management
+         contents = f.read()
+
+    num_words = get_num_words(contents)
+    output_list = (format(get_sorted_list(contents)))
 
     print(f"============ BOOKBOT ============ \n")
     print(f"Analyzing book found at {filepath}...\n")
     print(f"----------- Word Count ---------- \n")
     print(f"Found {num_words} total words \n")
     print(f"--------- Character Count -------\n")                            
-    print(f"{output_list}============= END ===============") # newline before END is part of output_list function
+    print(f"{output_list}============= END ===============")         # newline before END is part of output_list function
     return
